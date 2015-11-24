ce84d25c8f846a5e887c37899b32f606f4ec2dfd
jolive1993 <rockitshipshawty@gmail.com>
2015-11-24 11:21:30 -0600

    Added classes

---
+++ Functionality/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functionality
+{
+    public abstract class Vehicle
+    {
+        public int fuel = 0;
+    }
+}
